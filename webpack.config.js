101a5b1871bb3a1ce0c5bd0156c648af6dde55c4
Kuba <kubajmarek@gmail.com>
2019-08-23 20:47:40 +0200

    gh-pages updates

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   },
   output: {
     filename: '[name].[hash].js',
-    path: path.resolve(__dirname, './docs'),
+    path: path.resolve(__dirname, './dist'),
     publicPath: '/'
   },
   devServer: {
